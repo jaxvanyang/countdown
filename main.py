a592e447dae4429bc0e75bf1a29602dfc4352ac5
Jax Young <jaxvanyang@gmail.com>
2025-06-03 13:12:14 +0800

    feat: add support for setting hour and minute

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ class CountDown(Digits):
 
     DEFAULT_CSS = "CountDown { width: auto; }"
 
-    def __init__(self, year: int, month: int, day: int) -> None:
+    def __init__(self, year: int, month: int, day: int, hour: int, minute: int) -> None:
         super().__init__(self.DEFAULT_VALUE)
 
-        self.date = datetime(year, month, day)
+        self.date = datetime(year, month, day, hour, minute)
 
     def on_mount(self) -> None:
         self.update_value()
-        self.set_interval(1, self.update_value)
+        self.set_interval(1 / 60, self.update_value)
 
     def update_value(self) -> None:
         now = datetime.now()
@@ -35,17 +35,23 @@ class CountDown(Digits):
 
 class DateSelector(HorizontalGroup):
 
-    def __init__(self, year: int, month: int, day: int) -> None:
+    DEFAULT_CSS = ".hour { margin-left: 1; }"
+
+    def __init__(self, year: int, month: int, day: int, hour: int = 0, minute: int = 0) -> None:
         super().__init__()
 
         self.year = year
         self.month = month
         self.day = day
+        self.hour = hour
+        self.minute = minute
 
     def compose(self) -> ComposeResult:
         yield NumberSelector(2000, 2050, self.year)
         yield NumberSelector(1, 12, self.month)
         yield NumberSelector(1, 31, self.day)
+        yield NumberSelector(0, 23, self.hour, classes="hour")
+        yield NumberSelector(0, 59, self.minute)
 
 class NumberSelector(Widget):
 
@@ -57,8 +63,8 @@ class NumberSelector(Widget):
     }
     """
 
-    def __init__(self, min: int, max: int, default: int | None = None) -> None:
-        super().__init__()
+    def __init__(self, min: int, max: int, default: int | None = None, **kwargs) -> None:
+        super().__init__(**kwargs)
 
         self.min = min
         self.max = max
@@ -71,7 +77,7 @@ class NumberSelector(Widget):
 class CountDownApp(App):
 
     TITLE = "Count Down"
-    DEFAULT_DATE = (2026, 1, 1)
+    DEFAULT_DATE = [2026, 1, 1, 0, 0]
     CONFIG_PATH = Path(user_config_dir("countdown")) / "date.csv"
     BINDINGS = [
         ("q", "quit", "Quit")
@@ -83,7 +89,7 @@ class CountDownApp(App):
 
     #countdown {
         align-horizontal: center;
-
+        margin: 1;
     }
 
     DateSelector {
@@ -92,17 +98,24 @@ class CountDownApp(App):
     }
     """
 
-    def __init__(self, date: tuple[int, int, int] | None = None):
+    def __init__(self, date: list[int] | None = None):
         super().__init__()
+
+        date = date or self.DEFAULT_DATE
+        date.extend(self.DEFAULT_DATE[len(date):len(self.DEFAULT_DATE)])
 
         if self.CONFIG_PATH.is_file():
             with open(self.CONFIG_PATH) as f:
                 content = ",".join(f.readlines())
                 values = content.split(",")
-                if len(values) == 3:
-                    date = (int(values[0]), int(values[1]), int(values[2]))
 
-        self.date = date or self.DEFAULT_DATE
+            for i in range(0, 5):
+                if len(values) > i:
+                    date[i] = int(values[i])
+                else:
+                    break
+
+        self.date = date
 
     def compose(self) -> ComposeResult:
         yield HorizontalGroup(CountDown(*self.date), id="countdown")
